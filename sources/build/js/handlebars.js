d9ddd0c0564e549abe4244254eb8d20be2521028
Lissov <pavlo.lissov@gmail.com>
2025-08-30 10:25:46 +0200

    Update handlebars.js

--- sources/build/js/handlebars.js
+++ sources/build/js/handlebars.js
@@ -50,6 +50,6 @@ module.exports.registerHelpers = function() {
   Handlebars.registerHelper('addDays', function(day, delta, options) {
     const days = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag'];
     const i = days.indexOf(day);
-    return days[(i + delta) % days.Length] || 'incorrect';
+    return days[(i + delta) % 7] || 'incorrect ' + (i + delta) + " : " + ((i + delta) % 7));
   });
 }
